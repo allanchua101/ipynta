9e1e6cbb7373864ecf1cb421d9c3feb73e97e00c
Allan Chua <allanchua.officefiles@gmail.com>
2021-10-17 11:11:07 +0800

    Updated test cases

--- src/ipynta/tests/test_globbing.py
+++ src/ipynta/tests/test_globbing.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 SAMPLES_DIR = (Path(__file__).parent / "./imgs")
 
-def test_image_path():
+def test_get_img_paths():
     globber = ImageGlobber(SAMPLES_DIR)
     img_paths = globber.get_img_paths()
 
