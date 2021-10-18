c7b55ac0ab2bdeb26117011728872526ae0d325d
Allan Chua <allanchua.officefiles@gmail.com>
2021-10-18 13:42:56 +0800

    Jpeg tests

--- src/tests/globbing/test_globbing.py
+++ src/tests/globbing/test_globbing.py
@@ -7,7 +7,7 @@ def test_get_img_paths():
     globber = ImageGlobber(SAMPLES_DIR)
     img_paths = globber.get_img_paths()
     actual = len(img_paths)
-    expected = 1
+    expected = 2
 
     assert(actual == expected)
 
@@ -16,3 +16,9 @@ def test_get_img_paths_png():
     img_paths = globber.get_img_paths()
 
     assert(any(path.endswith(".png") for path in img_paths))
+
+def test_get_img_paths_jpg():
+    globber = ImageGlobber(SAMPLES_DIR)
+    img_paths = globber.get_img_paths()
+
+    assert(any(path.endswith(".jpg") for path in img_paths))
