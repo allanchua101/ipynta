49ebf715ffa5ebf0efaec94f1b2f524d82781afa
Allan Chua <allanchua.officefiles@gmail.com>
2021-10-20 00:26:00 +0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ else:
       long_description = "We're fixing our docs!"
 
 setup(name='ipynta',
-      version='0.0.15',
+      version='0.0.16',
       long_description=long_description,
       long_description_content_type='text/markdown',
       description="A Python library for different image processing tasks.",
