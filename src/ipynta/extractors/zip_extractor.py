3953d69fabb619ee7f73794fa97469f58f33f443
Allan Chua <allanchua.officefiles@gmail.com>
2021-10-24 22:12:33 +0800

    Optimized zip extractor

--- src/ipynta/extractors/zip_extractor.py
+++ src/ipynta/extractors/zip_extractor.py
@@ -1,5 +1,4 @@
-import zipfile
-import os
+from zipfile import ZipFile
 
 # Author: Allan Chua allanchua.officefiles@gmail.com
 
@@ -7,26 +6,23 @@ class ZipExtractor:
   """Class used for extracting zip file contents
   
   Attributes:
-    zip_file_path (str): path to the zip file.
-    destination_path (str): path to the destination folder.
+    zip_path (str): path of the zip file.
+    dest_path (str): path of the destination folder.
   """
-  def __init__(self, zip_file_path, destination_path):
-    self.zip_file_path = zip_file_path
-    self.destination_path = destination_path
+  def __init__(self, zip_path, dest_path):
+    self.zip_path = zip_path
+    self.dest_path = dest_path
 
   def execute(self):
-    """Execute the extraction of the zip file contents
+    """Executes the extraction of zip file contents
     
     Returns:
       list[str]: List of extracted file paths.
     """
-    extracted_files = []
+    output = []
 
-    with zipfile.ZipFile(self.zip_file_path, 'r') as zip_ref:
-      if not os.path.exists(self.destination_path):
-        os.makedirs(self.destination_path)
+    with ZipFile(self.zip_path) as zf:
+      zf.extractall(self.dest_path)
+      output = zf.namelist()
 
-      zip_ref.extractall(self.destination_path)
-      extracted_files = zip_ref.namelist()
-
-    return extracted_files
+    return output
